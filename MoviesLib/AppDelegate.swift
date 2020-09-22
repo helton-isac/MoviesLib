92ef10bd7b3c1d2e3ef6149dec2fe0b34e40af3f
Helton Isac <helton.isac@gmail.com>
2020-09-22 19:38:08 -0300

    Initial Commit

---
+++ MoviesLib/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  MoviesLib
+//
+//  Created by Helton Isac Torres Galindo on 22/09/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ MoviesLib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesLib
+//
+//  Created by Helton Isac Torres Galindo on 22/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

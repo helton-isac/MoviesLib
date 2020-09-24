6604a2d45e9a5904d77f004fca1e269af89326a6
Helton Isac <helton.isac@gmail.com>
2020-09-24 20:02:44 -0300

    Creating Movie Struct

---
+++ MoviesLib/MovieVisualization/Model/Movie.swift
@@ -0,0 +1,19 @@
+//
+//  Movie.swift
+//  MoviesLib
+//
+//  Created by Helton Isac Torres Galindo on 24/09/20.
+//
+
+import Foundation
+
+struct Movie {
+    
+    let title: String
+    let categories: String
+    let duration: String
+    let rating: Double
+    let summary: String
+    let image: String
+    
+}

e864d8b9d4d3fedb0a1a426e7b6baf923f813afc
Helton Isac <helton.isac@gmail.com>
2020-09-26 14:51:51 -0300

    Categories Delegate

--- MoviesLib/Categories/Controller/CategoriesTableViewController.swift
+++ MoviesLib/Categories/Controller/CategoriesTableViewController.swift
@@ -7,6 +7,11 @@
 //
 
 import UIKit
+import CoreData
+
+protocol CategoriesDelegate: class {
+    func setSelectedCategories(_ categories: Set<Category>)
+}
 
 class CategoriesTableViewController: UITableViewController {
 
@@ -14,16 +19,47 @@ class CategoriesTableViewController: UITableViewController {
         super.viewDidLoad()
     }
     
+    // MARK: Properties
+    var categories: [Category] = []
+    weak var delegate: CategoriesDelegate?
+    var selectedCategories: Set<Category> = [] {
+        didSet {
+            delegate?.setSelectedCategories(selectedCategories)
+        }
+    }
+    
     // MARK: - IBActions
     @IBAction func add(_ sender: Any) {
+
+    }
+    
+    // MARK: - Methods
+    private func loadCategories() {
+        let fetchRequest: NSFetchRequest<Category> = Category.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        do {
+            categories = try context.fetch(fetchRequest)
+            tableView.reloadData()
+        } catch  {
+            print(error)
+        }
     }
     
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let category = categories[indexPath.row]
+        cell.textLabel?.text = category.name
+        return cell
     }
 }

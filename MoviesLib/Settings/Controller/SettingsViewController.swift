ea4e35548052a226ba3ea3f9f98e4e33ebf50449
Helton Isac <helton.isac@gmail.com>
2020-09-26 17:32:39 -0300

    Settings User Preferences

--- MoviesLib/Settings/Controller/SettingsViewController.swift
+++ MoviesLib/Settings/Controller/SettingsViewController.swift
@@ -13,10 +13,10 @@ final class SettingsViewController: UIViewController {
     // MARK: - IBOutlets
     @IBOutlet var switchAutoPLay: UISwitch!
     @IBOutlet var segmentedControlColors: UISegmentedControl!
-    
     @IBOutlet var textFieldCategory: UITextField!
     
     // MARK: - Properties
+    private let ud = UserDefaults.standard
     
     
     // MARK: - Super Methods
@@ -24,17 +24,41 @@ final class SettingsViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupView()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    
     
     // MARK: - IBActions
     @IBAction func autoPlayChanged(_ sender: UISwitch) {
+        ud.set(sender.isOn, forKey: "autoplay")
     }
     @IBAction func colorChanged(_ sender: UISegmentedControl) {
+        ud.set(sender.selectedSegmentIndex, forKey: "color")
     }
     
     @IBAction func categoryChanged(_ sender: UITextField) {
+        ud.set(sender.text!, forKey: "category")
+        sender.resignFirstResponder()
     }
     
     // MARK: - Methods
-    
+    private func setupView() {
+        let autoplay = ud.bool(forKey: "autoplay")
+        switchAutoPLay.isOn = autoplay
+        
+        let colorIndex = ud.integer(forKey: "color")
+        segmentedControlColors.selectedSegmentIndex = colorIndex
+        
+        let category = ud.string(forKey: "category")
+        textFieldCategory.text = category
+        
+    }
     
 }

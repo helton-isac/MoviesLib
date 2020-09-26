b1c273cce78b67afe9ae421847958173aedc98e5
Helton Isac <helton.isac@gmail.com>
2020-09-26 14:29:17 -0300

    Creating Categories

---
+++ MoviesLib/Categories/Controller/CategoriesTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CategoriesTableViewController.swift
+//  MoviesLib
+//
+//  Created by Helton Isac Torres Galindo on 26/09/20.
+//  Copyright © 2020 FIAP. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - IBActions
+    @IBAction func add(_ sender: Any) {
+    }
+    
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}

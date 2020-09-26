adacc056ad0ec3a2f180212a8ffd22d3220c8698
Helton Isac <helton.isac@gmail.com>
2020-09-26 09:03:13 -0300

    Adding Database to the project

--- MoviesLib/Main/AppDelegate.swift
+++ MoviesLib/Main/AppDelegate.swift
@@ -6,18 +6,30 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window?.tintColor = UIColor(named: "Main")
         
         return true
     }
-
+    
+    // MARK: - Core Data Stack
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "MoviesLib")
+        container.loadPersistentStores{ (storeDescription, error) in
+            if let error = error {
+                print(error)
+            }
+        }
+        return container
+    }()
+    
 }
 

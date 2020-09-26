7563273677272e60efe8ab49ff10c91e48a961c0
Helton Isac <helton.isac@gmail.com>
2020-09-26 11:33:08 -0300

    Implementing Form Controller

---
+++ MoviesLib/MovieForm/Controller/MovieFormViewController.swift
@@ -0,0 +1,63 @@
+//
+//  MovieFormViewController.swift
+//  MoviesLib
+//
+//  Created by Helton Isac Torres Galindo on 26/09/20.
+//  Copyright © 2020 FIAP. All rights reserved.
+//
+
+import UIKit
+
+final class MovieFormViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet var textFieldTitle: UITextField!
+    @IBOutlet var textFieldRating: UITextField!
+    @IBOutlet var textFieldDuration: UITextField!
+    @IBOutlet var labelCategories: UILabel!
+    @IBOutlet var imageViewPoster: UIImageView!
+    @IBOutlet var textViewSummary: UITextView!
+    @IBOutlet var buttonSave: UIButton!
+    @IBOutlet var scrollView: UIScrollView!
+    
+    // MARK: - Properties
+    var movie: Movie?
+    
+    // MARK: - Super Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func selectImage(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+        
+    }
+    
+    // MARK: - Methods
+    @objc
+    private func keyboardWillShow(notification: NSNotification){
+        
+    }
+    
+    @objc
+    private func keyboardWillHide(){
+        
+    }
+    
+}
